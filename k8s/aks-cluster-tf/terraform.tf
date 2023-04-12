39ed9d5eb88c6b54dab2f4abcaf50b31bececf7b
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-04-12 15:03:27 +0200

    Fixed backend block

--- k8s/aks-cluster-tf/terraform.tf
+++ k8s/aks-cluster-tf/terraform.tf
@@ -1,6 +1,8 @@
-backend "azurerm" {
+terraform {
+  backend "azurerm" {
     resource_group_name  = "tf-rg"
     storage_account_name = "poznajtfstate"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
+}
