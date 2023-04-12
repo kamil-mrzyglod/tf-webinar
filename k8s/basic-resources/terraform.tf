78ec91fc1f167dd02456ffebe9fe2fea15e5d47c
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-04-12 15:46:30 +0200

    Added backend to the config

---
+++ k8s/basic-resources/terraform.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-rg"
+    storage_account_name = "poznajtfstate"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
