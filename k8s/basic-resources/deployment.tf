f6389f8011bc73667146da80369a323b291f8be0
Kamil Mrzygłód <kamil@thecloudtheory.com>
2023-04-12 20:40:56 +0200

    Added new deployment

--- k8s/basic-resources/deployment.tf
+++ k8s/basic-resources/deployment.tf
@@ -43,3 +43,49 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_deployment" "nginx2" {
+  metadata {
+    name = "scalable-nginx2-example"
+    labels = {
+      App = "ScalableNginxExample2"
+    }
+  }
+
+  spec {
+    replicas = 3
+    selector {
+      match_labels = {
+        App = "ScalableNginxExample2"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "ScalableNginxExample2"
+        }
+      }
+      spec {
+        container {
+          image = "nginx:1.7.8"
+          name  = "example"
+
+          port {
+            container_port = 80
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
+}
